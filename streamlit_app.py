9e9b528bbf43722f57b12d212b752359faeca98e
wol-ahead <william.lawrence@ahead.com>
2026-05-19 17:20:20 -0400

    Update streamlit_app.py
    Missed uncomment

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,8 +18,8 @@ cnx = st.connection("snowflake")
 
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
-#st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
