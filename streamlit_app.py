02ed888793cd4dd67b1d2ea153782cbdab8d16ec
wol-ahead <william.lawrence@ahead.com>
2026-05-19 17:25:26 -0400

    Update streamlit_app.py
    Missing character

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
       
         st.subheader(fruit_chosen + ' Nutritional Information')
-        smoothiefroot_response = requests.get("https://www.smoothiefroot.com/api/fruit/{search_on}")  
+        smoothiefroot_response = requests.get(f"https://www.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
